c05d2daf4461a566d87dc8b6d68b2e71bbec4412
Опросичев Алексей <leha.oprosichev@mail.ru>
2026-02-13 12:58:14 +0300

    rework get current user

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class UserData(BaseModel):
     age: int | None = Field(default=None, ge=0, le=110)
     registration_date: str | None = None 
     role: str = "Пользователь"
-
+    is_active: bool = True
 class UserCreate(UserBase):
     password: str
     user_data: UserData
